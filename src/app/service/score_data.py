1922fd5051401a336ac2f088049f923368d8417e
Polina Romanchenko <polinaromanchenko2000@gmail.com>
2022-12-11 12:05:15 +0300

    one more try 2

--- src/app/service/score_data.py
+++ src/app/service/score_data.py
@@ -1,47 +1,11 @@
 from tqdm import tqdm
-from bertopic import BERTopic
 from ..storage.articles_storage import get_all_cursor, update_tag
-
+from .model_inf import TopicModeling
 import pickle
-import re
-import spacy
-from nltk.corpus import stopwords
 
 model_runner = None
 # nlp = spacy.load('en_core_web_sm')
 
-#
-# class TopicModeling:
-#
-#     def __init__(self, path_to_model: str):
-#         self.bert_model = BERTopic.load(path_to_model)
-#         self.topic_dict = dict(zip(self.bert_model.get_topic_info()['Topic'],
-#                                    self.bert_model.get_topic_info()['Name']))
-#
-#     def text_preprocessing(self, text):
-#         if isinstance(text, str):
-#             text = [text]
-#         for idx, txt in enumerate(text):
-#             regex = re.compile('[A-Za-z]+')
-#             nlp = spacy.load('en_core_web_sm')
-#             mystopwords = stopwords.words('english') + ['paper', 'result', 'experiment', 'from', 'subject',
-#                                                         're', 'edu', 'use', 'data', 'method', 'based',
-#                                                         'new', 'approach', 'also', 'system', 'model',
-#                                                         'present', 'research', 'propose', 'base']
-#
-#             text[idx] = ' '.join(regex.findall(txt))
-#             doc = nlp(txt)
-#             text[idx] = ' '.join([token.lemma_ for token in doc])
-#             text[idx] = ' '.join([token for token in txt.split() if not token in mystopwords])
-#
-#         return text
-#
-#     def score_text(self, text):
-#         text = self.text_preprocessing(text)
-#         topics, prob = self.bert_model.transform(text)
-#         for idx, topic in enumerate(topics):
-#             topics[idx] = self.topic_dict[topic]
-#         return topics
 
 def score_data(logger):
     global model_runner
